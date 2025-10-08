1d1985716b04c0d16b197bfa9d52282a1a2cc595
gademers <guillaume.demers.gd@gmail.com>
2025-10-07 22:19:20 -0400

    fixed typo

--- home.nix
+++ home.nix
@@ -12,11 +12,11 @@ let
 in
 
 {
- #imports = [
+  #imports = [
   # /etc/home/smee/nixos-dotfiles/modules/neovim.nix
   #];
 
- home.username = "smee";
+  home.username = "smee";
   home.homeDirectory = "/home/smee";
   programs.git.enable = true;
   home.stateVersion = "25.05";
@@ -27,19 +27,19 @@ in
       nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos-smee";
     };
     initExtra = ''
-    	   export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
-	  '';
+          	   export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
+      	  '';
   };
 
   xdg.configFile = builtins.mapAttrs
     (name: subpath: {
-      source = create_symlink "${dotfiles}/${subpath};";
+      source = create_symlink "${dotfiles}/${subpath}";
       recursive = true;
     })
     configs;
 
   home.packages = with pkgs; [
-    
+
     #neovim stuff
     neovim
     ripgrep
@@ -51,9 +51,8 @@ in
     gcc
     rofi
     bitwarden
-    bitwarden-cli
     tealdeer
     xclip
     bat
-    ];
+  ];
 }
